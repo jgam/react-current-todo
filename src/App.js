361d398f7119de0bfe4446943dd5aba0293e833b
jgam <jgam@alumni.nd.edu>
2020-03-17 10:38:29 +0900

    todos implementing with basic components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 }
