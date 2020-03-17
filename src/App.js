9ab72ead6ffd5cce1346f2b7034edb55e0b2a5b4
jgam <jgam@alumni.nd.edu>
2020-03-17 11:18:13 +0900

    states building

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState }from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'learn the basics of react',
+      checked: true,
+    },
+    {
+      id:2,
+      text: 'component styling done',
+      checked: true,
+    },
+    {
+      id:3,
+      text: 'lets create scheduler management app',
+      checked: true,
+    },
+  ])
   return (
     <TodoTemplate>
       <TodoInsert />
