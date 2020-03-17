c58397d364728d9e4abec251911c4c9f49d4fabc
jgam <jgam@alumni.nd.edu>
2020-03-17 12:10:48 +0900

    added to do done

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import {MdAdd} from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, [])
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+            
+            //submit event prevents browsers from refreshing
+            //that is why we call preventdefulat
+            e.preventDefault();
+        },
+        [onInsert, value],
+    );
+
     return (
-        <form className="TodoInsert">
-            <input placeholder="Please input your todo" />
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input placeholder="Please input your todo" value={value} onChange={onChange} />
             <button type="submit">
                 <MdAdd />
             </button>
